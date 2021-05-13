c48a630d77b7c30def974944968b8a00d49c21ff
jj131204 <jj131204@gmail.com>
2021-05-13 23:09:19 +0000

    swap1

--- _swap.c
+++ _swap.c
@@ -15,7 +15,7 @@ void _swap(stack_t **stack, unsigned int line_number)
 
 	if (!stack || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%d:  can't swap, stack too short\n", line_number);
+		fprintf(stderr, "L%d: Need to use line_num\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	else
