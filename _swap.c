d647f641acd6585f6c5e37b2b7f5a2f4036b6cfd
jj131204 <jj131204@gmail.com>
2021-05-13 23:18:22 +0000

    swap2

--- _swap.c
+++ _swap.c
@@ -13,16 +13,14 @@ void _swap(stack_t **stack, unsigned int line_number)
 	int _stack = 0;
 	stack_t *tmp = *stack;
 
-	if (!stack || (*stack)->next == NULL)
+	if (!stack || (*stack)->next == NULL || !tmp)
 	{
 		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	else
-	{
-		_stack = tmp->n;
-		tmp->n = tmp->next->n;
-		tmp->next->n = _stack;
-	}
+	
+	_stack = tmp->n;
+	tmp->n = tmp->next->n;
+	tmp->next->n = _stack;
 
 }
