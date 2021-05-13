5db27da462921c673adb5711aaea3e3bff3a98c9
jj131204 <jj131204@gmail.com>
2021-05-13 20:42:06 +0000

    malloc error

--- _push.c
+++ _push.c
@@ -31,6 +31,7 @@ void _push(stack_t **stack, unsigned int line_num)
 	new_node = malloc(sizeof(stack_t));
 	if (!new_node)
 	{
+		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
 	n = atoi(value);
