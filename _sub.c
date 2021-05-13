a1c30fce46f01abd4bd9071ebd6244ffe8d4d9a1
jj131204 <jj131204@gmail.com>
2021-05-13 23:38:32 +0000

    betty

--- _sub.c
+++ _sub.c
@@ -1,21 +1,28 @@
 #include "monty.h"
 
+/**
+ * sub - Subtracts the top element of the stack
+ * from the second top element of the stack.
+ * @stack: Double pointer to the head of the Stack.
+ * @line_number: Number of the line.
+ */
+
 void _sub(stack_t **stack, unsigned int line_number)
 {
-	        stack_t *sum_node = *stack;
-        int add = 0;
+	stack_t *res_node = *stack;
+	int sub = 0;
 
-        if (!stack || (*stack)->next == NULL)
-        {
-                fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
-                exit(EXIT_FAILURE);
-        }
-        else
-        {
-                add += (*stack)->n - (*stack)->next->n;
-                (*stack)->next->n = add;
-                *stack = (*stack)->next;
-                free(sum_node);
-        }
+	if (!stack || (*stack)->next == NULL)
+	{
+	fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+	exit(EXIT_FAILURE);
+	}
+	else
+	{
+		sub += (*stack)->next->n - (*stack)->n;
+		(*stack)->next->n = sub;
+		*stack = (*stack)->next;
+		free(res_node);
+	}
 
 }
