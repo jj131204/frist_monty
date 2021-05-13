1e0e7c494096678a77713962af2e283cb0c088e4
jj131204 <jj131204@gmail.com>
2021-05-13 22:43:29 +0000

    test10

--- _pop.c
+++ _pop.c
@@ -11,15 +11,24 @@
 
 void _pop(stack_t **stack, unsigned int line_number)
 {
-	stack_t *tmp = *stack;
+	 stack_t *tmp = *stack;
 
 	if (!stack || !*stack)
-	{
-		dprintf(STDERR_FILENO, "L%d: can't pop an empty stack\n", line_number);
+        {
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		free_stack(*stack);
 		exit(EXIT_FAILURE);
 	}
-	*stack = tmp->next;
+
 	if (tmp->next)
+	{
 		tmp->next->prev = tmp->prev;
-	free(tmp);
+		(*stack)->prev = NULL;
+		free(tmp);
+	}
+	else
+	{
+		free(*stack);
+		*stack  = NULL;
+	}
 }
