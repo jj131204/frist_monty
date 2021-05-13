ef0bcaa85291fab871976de7c019a74dd43b4ff6
jj131204 <jj131204@gmail.com>
2021-05-13 16:35:20 +0000

    fix

--- _push.c
+++ _push.c
@@ -1,34 +1,6 @@
 #include "monty.h"
 
 /**
- * new_Node - Create new node.
- * @n: Is a value.
- * Return: New node.
- */
-
-stack_t *new_Node(int n)
-{
-	stack_t *node = NULL;
-
-	node = malloc(sizeof(stack_t));
-	if (node == NULL)
-	{
-		free(node);
-		exit(EXIT_FAILURE);
-	}
-	if (node == NULL)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
-		exit(EXIT_FAILURE);
-	}
-	node->n = n;
-	node->next = NULL;
-	node->prev = NULL;
-
-	return (node);
-}
-
-/**
  * _push - This function pushes an element to the stack.
  * @stack: Double pointer to the head of the Stack.
  * @line_num: Number of the line.
@@ -50,7 +22,8 @@ void _push(stack_t **stack, unsigned int line_num)
 	{
 		exit(EXIT_FAILURE);
 	}
-	new_node = new_Node(atoi(value));
+	n = atoi(value);
+	new_node->n = n;
 	new_node->next = (*stack);
 	new_node->prev = NULL;
 	if (*stack)
